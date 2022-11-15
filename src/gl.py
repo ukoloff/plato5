6049bc2d0e9fb4eef05ca4e4f4fe05540ae9a154
Stas Ukolov <ukoloff@gmail.com>
2022-11-15 11:28:19 +0500

    Paint triangle

--- src/gl.py
+++ src/gl.py
@@ -18,3 +18,12 @@ class GLX:
 
         GL.glClearColor(0.5, 0.5, 0.5, 1.0)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+
+        GL.glBegin(GL.GL_TRIANGLES)
+        GL.glColor(1, 0, 0)
+        GL.glVertex2f(0, 0)
+        GL.glColor(0, 1, 0)
+        GL.glVertex2f(0, 1)
+        GL.glColor(0, 0, 1)
+        GL.glVertex2f(1, 0)
+        GL.glEnd()
